2fe028c0e605a4b66cfe073125c8b27c57d36067
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2020-07-13 20:14:38 -0300

    Camada Service de lancamento implementada

--- minhasfinancas/src/main/java/com/fatecmm/minhasfinancas/service/LancamentoService.java
+++ minhasfinancas/src/main/java/com/fatecmm/minhasfinancas/service/LancamentoService.java
@@ -1,5 +1,6 @@
 package com.fatecmm.minhasfinancas.service;
 
+import com.fatecmm.minhasfinancas.exception.BusinessRuleException;
 import com.fatecmm.minhasfinancas.model.entity.Lancamento;
 import model.enums.StatusLancamento;
 
@@ -7,13 +8,15 @@ import java.util.List;
 
 public interface LancamentoService {
 
-    Lancamento salvar(Lancamento lancamento);
+    Lancamento salvar(Lancamento lancamento) throws BusinessRuleException;
 
-    Lancamento atualizar(Lancamento lancamento);
+    Lancamento atualizar(Lancamento lancamento) throws BusinessRuleException;
 
     void deletar(Lancamento lancamento);
 
     List<Lancamento> buscar(Lancamento lancamentoFiltro);
 
-    void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+    void atualizarStatus(Lancamento lancamento, StatusLancamento status) throws BusinessRuleException;
+
+    void validar(Lancamento lancamento) throws BusinessRuleException;
 }
--- minhasfinancas/src/main/java/com/fatecmm/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ minhasfinancas/src/main/java/com/fatecmm/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fatecmm.minhasfinancas.service.impl;
 
+import com.fatecmm.minhasfinancas.exception.BusinessRuleException;
 import com.fatecmm.minhasfinancas.model.entity.Lancamento;
 import com.fatecmm.minhasfinancas.model.repository.LancamentoRepository;
 import com.fatecmm.minhasfinancas.service.LancamentoService;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
@@ -24,14 +26,16 @@ public class LancamentoServiceImpl implements LancamentoService {
 
     @Override
     @Transactional
-    public Lancamento salvar(Lancamento lancamento) {
+    public Lancamento salvar(Lancamento lancamento) throws BusinessRuleException {
+        validar(lancamento);
         return repository.save(lancamento);
     }
 
     @Override
     @Transactional
-    public Lancamento atualizar(Lancamento lancamento) {
+    public Lancamento atualizar(Lancamento lancamento) throws BusinessRuleException {
         Objects.requireNonNull(lancamento.getId());
+        validar(lancamento);
         return repository.save(lancamento);
     }
 
@@ -52,8 +56,36 @@ public class LancamentoServiceImpl implements LancamentoService {
     }
 
     @Override
-    public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+    public void atualizarStatus(Lancamento lancamento, StatusLancamento status) throws BusinessRuleException {
         lancamento.setStatus(status);
+        lancamento.setStatus(StatusLancamento.PENDENTE);
         atualizar(lancamento);
+    }
+
+    @Override
+    public void validar(Lancamento lancamento) throws BusinessRuleException {
+        if(lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")){
+            throw new BusinessRuleException("Informs a valid description");
+        }
+
+        if(lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12){
+            throw new BusinessRuleException("Informs a valid month");
+        }
+
+        if(lancamento.getAno() == null || lancamento.getAno().toString().length() != 4){
+            throw new BusinessRuleException("Informs a valid year");
+        }
+
+        if(lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null){
+            throw new BusinessRuleException("Informs an user");
+        }
+
+        if(lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1){
+            throw new BusinessRuleException("Informs a valid value");
+        }
+
+        if(lancamento.getTipo() == null){
+            throw new BusinessRuleException("Enter a release type");
+        }
     }
 }
