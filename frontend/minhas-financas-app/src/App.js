849db6d7cda4d525640c934c4bea07eaeb7b37fa
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2020-07-19 14:47:45 -0300

    Frontend iniciado e estrutura do proj. modificado

---
+++ frontend/minhas-financas-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Primeiro Componente! ;) TCC está 1% pronto senhores
+    </div>
+  );
+}
+
+export default App;
